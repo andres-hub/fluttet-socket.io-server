1b7c99be3f2d5a74a9c47f02fe82b45a6a2e808d
andres-hub <andrescalderoncl@gmail.com>
2022-09-01 17:06:55 -0500

    ok hasta el video 39

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,20 @@
-const {io} = require('../index')
+const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Breaking Benjamin'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Héroes del Silencio'));
+bands.addBand(new Band('Metallica'));
 
 // Mensajes de sockets
 io.on('connection', client => {
-    console.log('Cliente conectado')
+    console.log('Cliente conectado');
+
+    client.emit('active-bands',bands.getBands())
+
     client.on('disconnect', () => { 
         console.log('Cliente desconectado')
      });
@@ -12,11 +24,25 @@ io.on('connection', client => {
         io.emit('mensaje', {admin: 'Nuevo mensaje'})
      });
 
+     client.on('vote-band', (payload)=>{
+      bands.voteBand(payload.id);
+      io.emit('active-bands',bands.getBands());
+     });
+
+     client.on('add-band', (payload)=>{
+      bands.addBand(new Band(payload.name));
+      io.emit('active-bands',bands.getBands());
+     });
+
+     client.on('delete-band', (payload)=>{
+      bands.deleteBand(payload.id);
+      io.emit('active-bands',bands.getBands());
+     });
+
    //   client.on('emitir-mensaje', (payload)=> {
-   //    console.log('emitir-mensaje')
+   //    console.log(payload)
    //    // io.emit('nuevo-mensaje', payload) emite a todos
    //    client.broadcast.emit('nuevo-mensaje', payload) // emite a todos menos al que lo emitio
    //   })
-
 
 });
